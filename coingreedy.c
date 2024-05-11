debd6b7a3340e886b975e7c45e7a4b115cbb6a16
RonitJariwala <143431555+RonitJariwala@users.noreply.github.com>
2024-05-11 21:31:57 +0530

    coingreedy.c

--- coingreedy.c
+++ coingreedy.c
@@ -1,29 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
 
-int denomination[]={1,2,5,10,20,50,100,500,1000};
-int n=sizeof(denomination)/sizeof(denomination[0]);
-
-int compare(const void *a, const void *b) {
-    return (*(int *)b -*(int *)a);
-}
-
-void findMin(int V){
-    qsort(denomination,n,sizeof(int),compare); 
-    int ans[100],ans_index=0,i;
+int coinChange(int coins[],int n,int amount){
+    int need=0,i;
     for(i=0;i<n;i++){
-        while(V>=denomination[i]){
-            V-=denomination[i];
-            ans[ans_index++]=denomination[i];
+        while(amount>=coins[i]){
+            amount=amount-coins[i];
+            need++;
         }
     }
-    for(i=0;i<ans_index;i++){
-        printf("%d ",ans[i]);
-    }
+    return need;
 }
 
 void main(){
-    int n=93;
-    printf("Following is minimal number of change for %d: ",n);
-    findMin(n);
+    int amt,n,i,min,*coins;	clrscr();
+    printf("Enter the number of coins:");
+    scanf("%d",&n);
+    *coins =(int*)malloc(n*sizeof(int));
+    printf("Enter the denominations of coins separated by space:");
+    for(i=0;i<n;i++){
+        scanf("%d",&coins[i]);
+    }
+    printf("Enter the amount to make change for:");
+    scanf("%d",&amt);
+    min=coinChange(coins,n,amt);
+    printf("Minimum number of coins required: %d\n",min);
+    free(coins);
+    getch();
 }
