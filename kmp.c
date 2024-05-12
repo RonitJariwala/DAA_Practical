0bb10d69816ab09769720503d08f26401f148ed8
RonitJariwala <143431555+RonitJariwala@users.noreply.github.com>
2024-05-12 15:18:35 +0530

    Update kmp.c

--- kmp.c
+++ kmp.c
@@ -1,52 +1,47 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
 
-void KMP_prefix(char* P, int m, int* pi) {
-    int i = 1, j = 0;
-    pi[0] = -1;
+void KMP_prefix(char* P,int m,int* pi){
+    int i=1,j=0;
+    pi[0]=-1;
 
-    while (i < m) {
-        while (j >= 0 && P[i] != P[j]) {
-            j = pi[j];
+    while(i<m){
+        while(j>=0 && P[i]!=P[j]){
+            j=pi[j];
         }
-        i++;
-        j++;
-        pi[i] = j;
+        i++; j++;
+        pi[i]=j;
     }
 }
 
-void KMP_match(char* P, char* T, int m, int n, int* pi) {
-    int i = 0, j = 0;
-
-    while (i < n) {
-        while (j >= 0 && T[i] != P[j]) {
-            j = pi[j];
+void KMP_match(char* P,char* T,int m,int n,int* pi){
+    int i=0,j=0;
+    while (i<n){
+        while (j>=0 && T[i]!=P[j]){
+            j=pi[j];
         }
-        i++;
-        j++;
-
-        if (j == m) {
-            printf("Pattern found at index %d\n", i - j);
-            j = pi[j];
+        i++; j++;
+        if (j==m){
+            printf("Pattern found at index %d\n",i-j);
+            j=pi[j];
         }
     }
-
-    if (j == m) {
-        printf("Pattern found at index %d\n", i - j);
-    } else {
+    if(j==m){
+        printf("Pattern found at index %d\n",i - j);
+    }else{
         printf("Pattern not found in text/Pattern Ended\n");
     }
 }
 
-int main() {
-    char text[] = "ABABDABACDABABCABAB";
-    char pattern[] = "AB";
-    int m = strlen(pattern);
-    int n = strlen(text);
-    int pi[m + 1];
+void main(){
+    int m,n,pi[100];
+    char text[]="RONIT";
+    char pattern[]="T";
+    m=strlen(pattern);
+    n=strlen(text);
+    pi[m + 1];
 
-    KMP_prefix(pattern, m, pi);
-    KMP_match(pattern, text, m, n, pi);
-
-    return 0;
+    KMP_prefix(pattern,m,pi);
+    KMP_match(pattern,text,m,n,pi);
 }
